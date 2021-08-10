7951c6494bc6ecbb60fd2f66af99ec8c8f09ca4b
ceelime <88331661+ceelime@users.noreply.github.com>
2021-08-10 09:23:00 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { withAuthenticator, AmplifySignOut, AmplifyS3Album } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
@@ -12,7 +12,7 @@ class App extends Component {
       <div className="App">
         <AmplifySignOut />
         <header className="App-header">
-      
+      <AmplifyS3Album />
           <p>
             Insert S3 Explorer here
           </p>
@@ -22,5 +22,7 @@ class App extends Component {
     );
   }
 }
+
+
 
 export default withAuthenticator(App);
